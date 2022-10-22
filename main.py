82f9c8790a9ee0fe949cfdd0cc87a249cabebf13
ponsun <ponsun@ponsundeMacBook-Pro.local>
2022-10-22 13:18:25 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+###这是一个获取Okay学习机开发者密码的脚本
+import hashlib
+import time
+import sys
+key = input("请输入您设备的序列号：")+ str(time.strftime("%Y",time.localtime()) )+str(int(time.strftime("%m", time.localtime()) ))+str(int(time.strftime("%d", time.localtime()) ))+"010@okay.cn"
+m = hashlib.md5()
+m.update(key.encode("utf8"))
+print("您设备的开发者密码为：",m.hexdigest()[-6:] )
+sys.exit()
